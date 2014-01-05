65e72df03cadf0deb8355102c5787374bd2fcb53
Grzegorz Bartman <grzegorz.bartman@droptica.com>
2014-01-05 18:35:11 +0100

    Issue #0: Superfish menu.

--- app/sites/all/themes/dtheme/templates/page.tpl.php
+++ app/sites/all/themes/dtheme/templates/page.tpl.php
@@ -70,6 +70,34 @@
 */ ?>
   </header>
 </div>
+<div id="mainmenu-over">
+  <div id="mainmenu" class="clearfix">
+
+  <?php /*  <?php if ($main_menu): ?>
+      <nav id="main-menu" role="navigation" tabindex="-1">
+        <?php
+        // This code snippet is hard to modify. We recommend turning off the
+        // "Main menu" on your sub-theme's settings form, deleting this PHP
+        // code block, and, instead, using the "Menu block" module.
+        // @see https://drupal.org/project/menu_block
+        print theme('links__system_main_menu', array(
+          'links' => $main_menu,
+          'attributes' => array(
+            'class' => array('links', 'inline', 'clearfix'),
+          ),
+          'heading' => array(
+            'text' => t('Main menu'),
+            'level' => 'h2',
+            'class' => array('element-invisible'),
+          ),
+        )); ?>
+      </nav>
+    <?php endif; ?> */?>
+
+    <?php print render($page['navigation']); ?>
+
+  </div>
+</div>
 <div id="main-over">
   <div id="main">
 
@@ -94,32 +122,7 @@
       <?php print $feed_icons; ?>
     </div>
 
-    <div id="navigation">
 
-      <?php if ($main_menu): ?>
-        <nav id="main-menu" role="navigation" tabindex="-1">
-          <?php
-          // This code snippet is hard to modify. We recommend turning off the
-          // "Main menu" on your sub-theme's settings form, deleting this PHP
-          // code block, and, instead, using the "Menu block" module.
-          // @see https://drupal.org/project/menu_block
-          print theme('links__system_main_menu', array(
-            'links' => $main_menu,
-            'attributes' => array(
-              'class' => array('links', 'inline', 'clearfix'),
-            ),
-            'heading' => array(
-              'text' => t('Main menu'),
-              'level' => 'h2',
-              'class' => array('element-invisible'),
-            ),
-          )); ?>
-        </nav>
-      <?php endif; ?>
-
-      <?php print render($page['navigation']); ?>
-
-    </div>
 
     <?php
       // Render the sidebars to see if there's anything in them.
