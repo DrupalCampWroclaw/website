0a7e4e7de89e6d32308d8246b55a232fbef25de2
Grzegorz Bartman <grzegorz.bartman@droptica.com>
2014-02-01 13:42:04 +0100

    Issue #0: Footer links.

--- app/sites/all/themes/dtheme/templates/page.tpl.php
+++ app/sites/all/themes/dtheme/templates/page.tpl.php
@@ -81,3 +81,15 @@
 </div>
 
 <?php print render($page['bottom']); ?>
+
+<div id="page-bottom-links-over">
+  <div id="page-bottom-links" class="block">
+  <a href="https://github.com/DrupalCampWroclaw" target="_blank">DrupalCamp Wroclaw on GitHub</a>
+   <br/>
+    <a href="http://2012.drupalcampwroclaw.pl/" target="_blank">DrupalCamp Wroclaw 2012</a>
+     | <a href="http://2013.drupalcampwroclaw.pl/"  target="_blank">DrupalCamp Wroclaw 2013</a>
+     | <a href="http://www.drupalday.pl/"  target="_blank">DrupalDay</a>
+     | <a href="http://www.drupalidzienastudia.pl/"  target="_blank">Drupal idzie na studia </a>
+     | <a href="https://groups.drupal.org/poland"  target="_blank">Drupal Groups - Poland</a>
+  </div>
+</div>
