c64ea650fdf7584034601248818c07130e50da03
Grzegorz Bartman <grzegorz.bartman@droptica.com>
2014-02-15 10:47:50 +0100

    Issue #0: Social media blocks.

---
+++ app/sites/all/modules/custom/layout/social-media.tpl.php
@@ -0,0 +1,24 @@
+<div id="social-media-widgets" class="clearfix">
+
+  <div id="social-media-twitter" class="social-media">
+    <a class="twitter-timeline" href="https://twitter.com/drupalcamp_wroc" data-widget-id="412852523758665728">Tweety na temat @drupalcamp_wroc</a>
+    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+  </div>
+
+  <div id="social-media-fb" class="social-media">
+    <iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fdrupalcampwroclawpl&amp;width&amp;height=360&amp;colorscheme=light&amp;show_faces=true&amp;header=true&amp;stream=false&amp;show_border=true&amp;appId=476413902469619" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:360px;" allowTransparency="true"></iframe>
+  </div>
+
+  <div id="social-media-gplus" class="social-media">
+    <!-- Place this tag where you want the widget to render. -->
+    <div class="g-page" data-href="//plus.google.com/104738857349518335066" data-rel="publisher"></div>
+    <!-- Place this tag after the last widget tag. -->
+    <script type="text/javascript" >
+      (function() {
+        var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+        po.src = 'https://apis.google.com/js/platform.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+      })();
+    </script>
+  </div>
+</div>
