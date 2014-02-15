cab96a9af27bbdc8105519f35743739194940b67
Grzegorz Bartman <grzegorz.bartman@droptica.com>
2014-02-15 11:30:03 +0100

    Issue #0: Flickr gallery.

---
+++ app/sites/all/modules/custom/layout/flickr.tpl.php
@@ -0,0 +1,30 @@
+<div id="flickr-gallery-widger" class="clearfix">
+  <!-- Start of Flickr Badge -->
+  <style type="text/css">
+  /*
+  Images are wrapped in divs classed "flickr_badge_image" with ids "flickr_badge_imageX" where "X" is an integer specifying ordinal position. Below are some styles to get you started!
+  */
+  #flickr_badge_uber_wrapper {text-align:center; width:260px;}
+  #flickr_badge_wrapper {padding:10px 0 10px 0;}
+  .flickr_badge_image {margin:0 10px 10px 10px;}
+  .flickr_badge_image img {border: 1px solid black !important;}
+  #flickr_badge_source {text-align:left; margin:0 10px 0 10px;}
+  #flickr_badge_icon {float:left; margin-right:5px;}
+  #flickr_www {display:block; padding:0 10px 0 10px !important; font: 11px Arial, Helvetica, Sans serif !important; color:#3993ff !important;}
+  #flickr_badge_uber_wrapper a:hover,
+  #flickr_badge_uber_wrapper a:link,
+  #flickr_badge_uber_wrapper a:active,
+  #flickr_badge_uber_wrapper a:visited {text-decoration:none !important; background:inherit !important;color:#3993ff;}
+  #flickr_badge_wrapper {}
+  #flickr_badge_source {padding:0 !important; font: 11px Arial, Helvetica, Sans serif !important; color:#666666 !important;}
+  </style>
+  <div id="flickr_badge_uber_wrapper">
+    <div id="flickr_badge_wrapper">
+      <script type="text/javascript" src="http://www.flickr.com/badge_code_v2.gne?show_name=1&count=8&display=latest&size=m&layout=x&context=in%2Fpool-drupalcampwroclaw%2F&source=group&group=2453588%40N21"></script>
+    </div>
+  </div>
+  <div id="flickr_badge_source">
+    <a href="http://www.flickr.com/groups/drupalcampwroclaw/pool/" target="blank"><img id="flickr_badge_icon" alt="items in DrupalCamp Wroclaw" src="https://farm3.staticflickr.com/2842/buddyicons/2453588@N21.jpg?1387024994" /></a><span id="flickr_badge_source_txt">More <a href="http://www.flickr.com/groups/drupalcampwroclaw/pool/">in DrupalCamp Wroclaw pool</a></span><br clear="all" />
+  </div>
+  <!-- End of Flickr Badge -->
+</div>
