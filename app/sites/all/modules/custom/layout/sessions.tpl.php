db60d505c24d5ab377497e6a6d25ed889c5c1040
Grzegorz Bartman <grzegorz.bartman@droptica.com>
2014-03-01 10:10:02 +0100

    Issue #0: Sessions block.

--- app/sites/all/modules/custom/layout/sessions.tpl.php
+++ app/sites/all/modules/custom/layout/sessions.tpl.php
@@ -2,6 +2,6 @@
   <ul>
     <li><?php print l(t('Proposed Sessions'), 'sessions/proposed'); ?></li>
     <li><?php print l(t('Accepted Sessions'), 'sessions/accepted'); ?></li>
-    <li><?php print l(t('Submit your session!'), 'node/add/sessions'); ?></li>
+    <li><?php print l(t('Submit your session!'), 'node/add/session'); ?></li>
   </ul>
 </div>
